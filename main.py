ad94ebea5a18b438d7810da4f60b1f4c4a58e9b4
zosiabarczak <145969323+zosiabarczak@users.noreply.github.com>
2023-10-10 19:18:44 +0000

    Mój pierwszy program
    Program pyta użytkownika o wiek i wykorzystująć instrukcję warunkowe,
    wypisuje informacje w zależności od podanego wieku.

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-if liczba == 1:
-    print('jeden')
-    print('koniec 1')
+#!/usr/bin/env python3
+
+print("Hello, tu mój program") # Wypisz na ekran
+wiek = input("Podaj swój wiek\n") # Pobierz ciąg znaków, zwrócona zmina jst typu string 
+wiek = int(wiek) # Zrob konwersje typu zmiennej z string na integer 
+
+# Wypisz na ekran użytkownikowi co podał, dokonana jest konwersja typu na string
+print("Podałeś, że twój wiek to: " + str(wiek) + ". Nieźle.")
+
+if wiek < 18:
+    print('Brawo, jesteś niepełnoletni')
+elif wiek < 60:
+    print('Juz jestes pełnoletni i możesz pracować')
+elif wiek > 60:
+    print('Juz jestes na emeruturze')
+elif wiek > 100 and wiek < 120:
+    print('Uuu, rekord życia')
+else:
+    print('Chyba coś kłamiesz')
